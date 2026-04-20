ffc73df7a9078b38fe5ddda48bf8d6a46d035e99
Alex <alexrosenk@hotmail.se>
2026-04-21 00:35:13 +0200

    fix: JwtAuthFilter was missing bean annotation

--- backend/src/main/java/ar/ss/betting/security/JwtAuthFilter.java
+++ backend/src/main/java/ar/ss/betting/security/JwtAuthFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ import java.io.IOException;
 // Adds principal object to SecurityContextHolder directly,
 // so that subsequent UsernamePasswordAuthenticationFilter sees the principal and is
 // effectively skipped
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final CustomUserDetailsService customUserDetailsService;
